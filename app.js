7585251dba59eb59a3119b383d5012b6c7356e50
Akshat Tekriwal <akshattekriwal@Akshats-MacBook-Pro.local>
2019-02-01 18:15:59 -0500

    connection successful

--- app.js
+++ app.js
@@ -3,40 +3,42 @@ var express = require('express'),
     path = require("path"),
     request = require("request");
     mysql      = require('mysql');
-    bodyparser = require('body-parser')
 
 var app = express();
-app.use(bodyparser.json());
-
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'Domain123',
-  database : 'draft1'
-});
-
-connection.connect();
- 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
-
-connection.query('SELECT * FROM student', function (error, results, fields) {
-    // error will be an Error if one occurred during the query
-    // results will contain the results of the query
-    // fields will contain information about the returned results fields (if any)
-    console.log(result);
-  });
 
 // create application/json parser
-app.use(bodyParser.json())
+//app.use(bodyParser.json())
 
 // create application/x-www-form-urlencoded parser
 app.use(bodyParser.urlencoded({ extended: false }))
 
 //Set static path for ststic resources
 app.use(express.static(path.join(__dirname, "public")));
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Domain123',
+    database : 'draft1'
+  });
+  
+  connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+   
+    console.log('connected as id ' + connection.threadId);
+  });
+  
+  
+  connection.query('SELECT * FROM student where  gpa >= 2.50', function (error, results, fields) {
+      // error will be an Error if one occurred during the query
+      // results will contain the results of the query
+      // fields will contain information about the returned results fields (if any)
+      console.log(results[0].name);
+    });
+  
 
 
 app.get("/", function (req, res) {
@@ -55,26 +57,26 @@ app.get("/", function (req, res) {
             console.log("some mannual error 1");
 
         var courseListString = "";
-        var course = "AASP398Q,AASP398Z,AASP402,AASP441,AASP443,AASP478N,AASP498J";        
-        
-        //console.log("the course is " + course);
+        var course = "AASP398Q,AASP398Z,AASP402,AASP441,AASP443,AASP478N,AASP498J";           
 
         courseList.forEach(x => {
             courseListString = courseListString + x + ",";
-        });
+        });     
 
-        //console.log(courseListString);
-
-        request("https://api.umd.io/v0/courses/"+course, function (error, response, body1) {
+        request("https://api.umd.io/v0/courses/cmsc132?expand=sections", function (error, response, body) {
             console.log("connectiong with umd id sections");    
-        // var parsedData1 = JSON.parse(body1);
-            res.send(body1);
+        var parsedData1 = JSON.parse(body);
+
+           console.log(parsedData1.sections[0].meetings[0].room);
+        res.send(body);
+
         });
     });
 
 });
 
+connection.end();
 
-app.listen(3010, function () {
+app.listen(3000, function () {
     console.log("server started at port 3000");
 });
