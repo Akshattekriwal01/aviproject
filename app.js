65e4c230b9860e4fe31e4da5daaf0d19e0a2e72c
Akshattekriwal01 <akshattekriwal123@gmail.com>
2019-05-06 16:34:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -32,75 +32,7 @@ var connection = mysql.createConnection({
     console.log('connected as id ' + connection.threadId);
   });
   
-  
-//   connection.query('SELECT * FROM student where  gpa >= 2.50', function (error, results, fields) {
-//       // error will be an Error if one occurred during the query
-//       // results will contain the results of the query
-//       // fields will contain information about the returned results fields (if any)
-//       console.log(results[0].name);
-//     });
-  
-
-// Requesting and stroing data into the array
-
-
-// app.get("/", function (req, res) {
-//         var courseList = [];
-//         request("https://api.umd.io/v0/courses/list", function (error, response, body) {
-
-//             if (!error) {
-//                 var parsedData = JSON.parse(body);
-//                 //adding each course to "courseList"
-//                 parsedData.forEach(function (element) {
-//                     courseList.push(element["course_id"]);
-//                 });
-//                 console.log(" 1 length of array is:" + courseList.length);
-//             }
-//             else
-//                 console.log("some mannual error 1");
-
-//             var courseListString = "";
-
-//             courseList.forEach(x => {
-//                 courseListString = courseListString + x + ",";
-//             });     
-//             request("https://api.umd.io/v0/courses/astr100?expand=sections", function (error, response, body) {
-//                 console.log("connectiong with umd id sections");    
-//             var parsedData1 = JSON.parse(body);
-//             var allData=[];
-//             for(var i  = 0; i < parsedData1.sections.length; i++){
-//                 for(var j = 0; j < parsedData1.sections[i].meetings.length ; j++)
-//                 {   var objArray = [];              
-//                     if(j==0){                    
-//                         objArray.push(parsedData1.sections[i].section_id+"L");
-//                     }
-//                 else{
-//                         objArray.push(parsedData1.sections[i].section_id+"D");
-//                 }
-//                     objArray.push(parsedData1.sections[i].meetings[j].days);
-//                     objArray.push(Number(manageTime(parsedData1.sections[i].meetings[j].start_time)));
-//                     objArray.push(Number(manageTime(parsedData1.sections[i].meetings[j].end_time)));
-//                     objArray.push(parsedData1.sections[i].meetings[j].building);
-//                     objArray.push(parsedData1.sections[i].meetings[j].room);
-                
-//                 allData.push(objArray); 
-//                     // console.log("===================")
-//                     // for(var c = 0 ; c< objArray.length; c++)    
-//                     // console.log(objArray[c]);
-//                     // console.log("===================")
-//                 }
-        
-//             }  
-//             var l =0;
-//             while( l < allData.length){
-//                 console.log(allData[l]);           
-//                 l++;
-//             }
-//             res.send(body);
-//             });
-//         });
-
-//     });
+ 
 var allData=[];
 var courseList = [];
 app.get("/",courseArray,addToDatabase);
